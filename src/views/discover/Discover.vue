9cf5e16256c0b59c53108cb6806af517d03f8665
Hugo Scarbonchi <hugoscarbonchi@gmail.com>
2020-12-08 12:55:02 +0100

    Update

--- src/views/discover/Discover.vue
+++ src/views/discover/Discover.vue
@@ -1,12 +1,63 @@
-<template>
-  <div id="app"></div>
-  <h1>TEST</h1>
-  <div v-for="sport in sports" v-bind:key="sport.id">
-    <h1>{{ sport.name }}</h1>
-    <h3>#{{ sport.primary_color }}</h3>
-    <h3>#{{ sport.secondary_color }}</h3>
-  </div>
 
+<template>
+  <div id="app">
+  <section id="s1" class=" row wrap blue">
+    <img id="circle" src="../../assets/media/img/circle.svg">
+    <div class="col-10 offset-1 red start-center">
+
+
+      <div id="container_s1" class="purple title">
+        <h1 id="titrel1">Découvrir notre concept</h1>
+        <h1 id="titrel2">Le projet Open Innov EPSI</h1>
+        <div class="blue subtitle">
+          <p>
+            Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.
+            Nulla quis lorem ut libero malesuada feugiat. Mauris blandit
+            aliquet elit, eget tincidunt nibh pulvinar a. Sed porttitor lectus nibh
+            . Curabitur non nulla sit amet nisl tempus convallis quis ac lectus.
+          </p>
+        </div>
+        <div class="green center col-6">
+          <img  src="../../assets/media/img/preview.png">
+        </div>
+
+      </div>
+
+    </div>
+
+  </section>
+
+  <section>
+    <div>
+      <div>
+
+      </div>
+    </div>
+    <div>
+      <div>
+
+      </div>
+    </div>
+  </section>
+
+<section>
+  <div>
+    <div>
+
+    </div>
+  </div>
+  <div>
+    <div>
+
+    </div>
+  </div>
+</section>
+
+
+
+
+  </div>
 </template>
+
 <style lang="css" src="./discover.css"></style>
 <script src="./discover.js"></script>
